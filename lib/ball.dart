b343b95c6f2b55086a73909e36bd9aab378851a6
Andre Johnson <andre.johnson@stud-mail.uni-wuerzburg.de>
2021-03-06 17:57:22 +0100

    Left and Right button

--- lib/ball.dart
+++ lib/ball.dart
@@ -7,7 +7,28 @@ class BallGame extends StatefulWidget {
   }
 }
 
+enum _Direction {
+  LEFT,
+  RIGHT,
+  STOP
+}
+
 class _BallGameState extends State<BallGame> {
+
+  int ballPos0 = 4;
+  int ballPos1 = 2;
+  int ballPos2 = 3;
+
+  var ballDir0 = _Direction.LEFT;
+  var ballDir1 = _Direction.RIGHT;
+  var ballDir2 = _Direction.RIGHT;
+
+  var reflected0 = false;
+  var reflected1 = false;
+  var reflected2 = false;
+
+  var handPos = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,28 +37,35 @@ class _BallGameState extends State<BallGame> {
           children: [
             Column(
               children: [
-                hand(false),
-                hand(true),
-                hand(false),
+                hand(handPos == 0),
+                hand(handPos == 1),
+                hand(handPos == 2),
               ],
             ),
             Column(
-              children: [arc(12, 3), arc(10, 5), arc(8, 2)],
+              children: [
+                arc(12,
+                    ballPos0),
+                arc(10,
+                    ballPos1),
+                arc(8,
+                    ballPos2)
+              ],
             ),
             Column(
               children: [
-                hand(false),
-                hand(true),
-                hand(false),
+                hand(handPos == 0),
+                hand(handPos == 1),
+                hand(handPos == 2),
               ],
             ),
           ],
         ),
         Row(
           children: [
-            Text("Button <"),
+            ElevatedButton(onPressed: leftClick, child: Text("<")),
             Text("Button step()"),
-            Text("Button >")
+            ElevatedButton(onPressed: rightClick, child: Text(">"))
           ],
         )
       ],
@@ -50,6 +78,28 @@ class _BallGameState extends State<BallGame> {
 
   Widget hand(bool active) {
     return Text(active ? '||||' : '|  |');
+  }
+
+  void leftClick() {
+    if (handPos > 0) {
+      setState(() {
+        handPos--;
+      });
+    }
+  }
+
+  void rightClick() {
+    if (handPos < 2) {
+      setState(() {
+        handPos++;
+      });
+    }
+  }
+
+  void step() {
+    setState(() {
+
+    });
   }
 }
 
