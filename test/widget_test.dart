a46c24c8536be696d4a049dcb03f62bc0c8f9d1f
apfelsinecode <apfelsinemail@gmx.de>
2021-03-13 21:11:56 +0100

    renamed title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_test_eins/main.dart';
+import 'package:game_and_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
